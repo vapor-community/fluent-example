dca6a2d72155d47bdab1150beb17cdf4518094ef
Tanner Nelson <tanner@bluebite.com>
2016-02-01 18:45:08 -0500

    remove vapor

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,34 +1,11 @@
-import Vapor
-
 import Fluent
 import SQLiteDriver
 
 Database.driver = SQLiteDriver()
 
-Route.get("users") { request in
+if let user = User.find(1) {
+	print(user.name)
 
-	if let user = User.find(1) {
-		return user.name
-	} else {
-		return "No user with id 1"
-	}
-
-}
-
-Route.post("users") { request in
-
-	guard let user = User.find(1) else {
-		return "No user with id 1"
-	}
-	guard let name = request.data["name"] else {
-		return "please put a name"
-	}
-
-	user.name = name
+	user.name = "Test'"
 	user.save()
-
-	return "done"
 }
-
-let server = Server()
-server.run(port: 8080)
