24a29a4696a7d32f7f57bbe7762f82640577b2e0
Tanner Nelson <tanner@bluebite.com>
2016-02-21 17:25:03 -0500

    naming

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentApp",
     dependencies: [
-        //.Package(url: "https://github.com/tannernelson/vapor.git", majorVersion: 0),
-        .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0),
-        .Package(url: "https://github.com/tannernelson/fluent-sqlite.git", majorVersion: 0),
+        .Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0),
+        .Package(url: "https://github.com/qutheory/fluent-sqlite.git", majorVersion: 0),
     ]
 )
